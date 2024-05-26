1eaa88e9d9ab4cbf7e60b7138ee89505d44b7ecc
Teplovozovich <artur.dmitriev.2016@inbox.ru>
2024-05-26 12:33:32 +0500

    feat: add horizontal scroll

--- src/components/SettingsPage/SettingsPage.js
+++ src/components/SettingsPage/SettingsPage.js
@@ -2,38 +2,43 @@ import React, { useEffect, useState } from 'react';
 import s from './SettingsPage.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { setBackSideCard, setSumCards } from '../../redux/reducers/cardsSlice';
+import "./Settings.css"
 
 const SettingsPage = () => {
   const backs = useSelector((state) => state.cards.backsCardSide);
-  let sumCards = useSelector((state) => state.cards.sumCards);
+  const [cardCount, setCardCount] = useState('');
   const dispatch = useDispatch();
-  console.log(backs);
 
   const handleBackSideCardClick = (e) => {
     const chosenBack = e.target.getAttribute('src');
     dispatch(setBackSideCard(chosenBack));
   }
 
-  const [cardCount, setCardCount] = useState('');
-
   const handleInputChange = (e) => {
     const count = e.target.value;
     setCardCount(count);
   }
 
-  const handleClick = (e) => {
+  const handleButtinClick = (e) => {
     console.log('Количество карточек:', cardCount);
-    dispatch(setSumCards(cardCount))
+    if (cardCount > 4999 || cardCount < 2) {
+      alert("Вы ввели неправильное колличество")
+    } else {
+      dispatch(setSumCards(cardCount))
+
+    }
   }
 
   return (
-    <div>
-      {backs.map(back => (
-        <img key={back} className={s.back_side_card} src={back} onClick={handleBackSideCardClick} />
-      ))}
-      <p>Введите количество карточек:</p>
+    <div className={s.settings}>
+      <div className={s.back_side_card_wrapper}>
+        {backs.map(back => (
+          <img key={back} className={s.back_side_card} src={back} onClick={handleBackSideCardClick} />
+        ))}
+      </div>
+      <p>Введите количество пар:</p>
       <input type="number" value={cardCount} onChange={handleInputChange} />
-      <button onClick={handleClick}>выбрать</button>
+      <button onClick={handleButtinClick}>выбрать</button>
     </div>
   )
 };
